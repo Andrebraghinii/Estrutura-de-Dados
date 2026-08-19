1eb5d1b884dce3b247fa24a47bf28c5581dcef8a
Andre Nascimento <andrefelipebn1@gmail.com>
2026-08-19 19:37:13 -0300

    Vetores

---
+++ Atividades/palavra/Program.cs
@@ -0,0 +1,23 @@
+﻿string firstName = "André";
+string lastName = "Nascimento";
+
+string note = lastName.ToUpper() + " " + firstName;
+
+string initials = lastName[0] + " " + firstName[0];
+
+
+Console.WriteLine ("" + initials);
+
+// Formatação de String
+string texto = string.Format ("{0} {1} nascido em {2}", firstName, lastName, "1984");
+
+Console.WriteLine (texto);
+
+//C# é uma linguagem filha do c++
+//totalmente orientada a ogjetos
+//portanto tudo dentro do c é descendente
+//do tipo object
+
+int age = 24;
+object ageBoxing = age;
+int ageUnboxing = (int) ageBoxing;
