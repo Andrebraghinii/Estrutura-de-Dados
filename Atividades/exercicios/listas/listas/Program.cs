c32fade0f02e1bec8ab6cd2ce88d09f7eb7a77dc
Andre Nascimento <andrefelipebn1@gmail.com>
2026-08-19 22:07:01 -0300

    listas aula 19

---
+++ Atividades/exercicios/listas/listas/Program.cs
@@ -0,0 +1,21 @@
+﻿//arrayLists
+
+using System.Collections;
+
+ArrayList  arrayList = new ArrayList();
+
+//adicionando itens a lista
+arrayList.Add(5);
+
+//adicionando quantia de itens na lista
+arrayList.AddRange(new int[]{1, 2, 3});
+
+//
+arrayList.Insert(3, 7.8);
+
+//percorrendo itens da lista de forma generica
+
+foreach (object obj in arrayList){
+    
+    Console.WriteLine(obj);
+}
